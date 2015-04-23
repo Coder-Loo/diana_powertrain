6265a46e3f0c3205b312411fc3eb45723e3f3382
clynamen <clynamen@gmail.com>
2015-04-23 14:53:12 +0200

    fix prompt sdo key

--- include/diana_powertrain/consts.hpp
+++ include/diana_powertrain/consts.hpp
@@ -3,7 +3,7 @@
 
 #include <hlcanopen/types.hpp>
 
-hlcanopen::SDOIndex  OS_COMMAND_PROMPT(0x1023, 0);
+hlcanopen::SDOIndex  OS_COMMAND_PROMPT(0x1023, 1);
 hlcanopen::SDOIndex  OS_COMMAND_MODE(0x1024, 0);
 
 #endif
